19c6b259f8ed12a7e45d8bbc37770af6ddb41bd5
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2018-10-08 15:10:19 -0700

    Update Fibonacci.java
    extra description

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,6 +4,7 @@ public class Fibonacci {
 	public static void main(String[] args) {
 		// write a for loop that computes and prints 12 fibonacci nrs
 		//xn = xn-1 + xn-2
+		// Fib Nr:list starts with 1 1 and each next is a sum of two nrs before it
 		int x = 1;
 		int y = 1; 
 		int z;
