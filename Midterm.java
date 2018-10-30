c214392c7b35d878b096585c94a240106c8b7730
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2018-10-30 14:21:12 -0700

    Update Midterm.java
    copy paste since it was not completely uploaded in the first place

--- Midterm.java
+++ Midterm.java
@@ -1,9 +1,38 @@
 
+import java.util.Random;
+
 public class Midterm {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		//step need to be converted to show random nr between 1 or -1
+		Random r = new Random();
+		
+		randomWalk();
 	}
+public static void randomWalk() {
+	int step = 0;
+	int position = 0;
+	
+	
+	
+	int step1  = rand.nextBooleen(){
+			if (rand = true){step1 = 1
+			} else step1 = -1;
+			
+	}
+	while (position != 3 && position != -3) {
+		
+		position = position + step1;
+		step1 ++;
+		
+	}
+	System.out.print("max position = " + max );
+}
+public static int max(int position) {
+	//figure out max by comparing position with new position
+	
+}
 
 }
